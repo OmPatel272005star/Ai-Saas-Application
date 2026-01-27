755274e29ac3c451193d46d92e58b93bd9810b19
Om Patel <123579689+OmPatel272005star@users.noreply.github.com>
2026-01-27 08:46:36 +0530

    Delete server directory

--- server/configs/db.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-import { neon } from "@neondatabase/serverless";
-
-// Load .env file
-dotenv.config();
-
-const sql = neon(process.env.DATABASE_URL);
-
-export default sql;
